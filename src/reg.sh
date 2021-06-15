3018124ac4aff4fabf215aba8f74a8e543c7c7fe
gregblt <gregorybliault@gmail.com>
2021-06-15 10:14:25 +0200

    reg can now be done with nifti or dicoms

--- src/reg.sh
+++ src/reg.sh
@@ -5,16 +5,20 @@ pre=$1
 post=$2
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/slicer/lib/Slicer-4.8/cli-modules/
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/slicer/lib/Slicer-4.8/
-# Prepare inputs
-mkdir /tmp/input
-mkdir /tmp/output
-mkdir /tmp/pre
-cd /tmp/pre
-tar -xvzf "$pre"
-/src/dcm2niix -f pre -o /tmp/input/ /tmp/pre
-mkdir /tmp/post
-cd /tmp/post
-tar -xvzf "$post"
-/src/dcm2niix -f post -o /tmp/input/ /tmp/post
+# If niftis
+if [ "${pre: -4}" == ".nii" ] || [ "${pre: -7}" == ".nii.gz" ]; then
+  /opt/slicer/lib/Slicer-4.8/cli-modules/BRAINSFit --fixedVolume "$pre" --movingVolume "$post"   --useRigid --outputVolume "/output/output.nii.gz" --outputTransform "/output/transform.hdf5" --initializeTransformMode useGeometryAlign --samplingPercentage 0.1
+else
+  mkdir /tmp/input
+  mkdir /tmp/output
+  mkdir /tmp/pre
+  cd /tmp/pre
+  tar -xvzf "$pre"
+  /src/dcm2niix -f pre -o /tmp/input/ /tmp/pre
+  mkdir /tmp/post
+  cd /tmp/post
+  tar -xvzf "$post"
+  /src/dcm2niix -f post -o /tmp/input/ /tmp/post
 
-/opt/slicer/lib/Slicer-4.8/cli-modules/BRAINSFit --fixedVolume /tmp/input/pre.nii --movingVolume /tmp/input/post.nii   --useRigid --outputVolume "/output/output.nii.gz" --outputTransform "/output/transform.hdf5" --initializeTransformMode useGeometryAlign --samplingPercentage 0.1
+  /opt/slicer/lib/Slicer-4.8/cli-modules/BRAINSFit --fixedVolume /tmp/input/pre.nii --movingVolume /tmp/input/post.nii   --useRigid --outputVolume "/output/output.nii.gz" --outputTransform "/output/transform.hdf5" --initializeTransformMode useGeometryAlign --samplingPercentage 0.1
+fi
